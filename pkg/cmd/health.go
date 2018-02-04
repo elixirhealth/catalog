1d6749b942fb5a956390581daa3e8b4bf26eda27
Drausin Wulsin <daedalus2718@gmail.com>
2018-02-04 14:43:25 -0500

    rm libri/server dep

--- pkg/cmd/health.go
+++ pkg/cmd/health.go
@@ -2,10 +2,10 @@ package cmd
 
 import (
 	"log"
+	"net"
 	"os"
 
 	lserver "github.com/drausin/libri/libri/common/logging"
-	server2 "github.com/drausin/libri/libri/librarian/server"
 	"github.com/elxirhealth/service-base/pkg/server"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -30,10 +30,24 @@ func init() {
 }
 
 func getHealthChecker() (server.HealthChecker, error) {
-	addrs, err := server2.ParseAddrs(viper.GetStringSlice(catalogsFlag))
+	addrs, err := parseAddrs(viper.GetStringSlice(catalogsFlag))
 	if err != nil {
 		return nil, err
 	}
 	lg := lserver.NewDevLogger(lserver.GetLogLevel(viper.GetString(logLevelFlag)))
 	return server.NewHealthChecker(server.NewInsecureDialer(), addrs, lg)
+}
+
+// parseAddrs parses an array of net.TCPAddrs from an array of IPv4:Port address strings.
+// TODO (drausin) move to libri lib once separated from server pkg
+func parseAddrs(addrs []string) ([]*net.TCPAddr, error) {
+	netAddrs := make([]*net.TCPAddr, len(addrs))
+	for i, a := range addrs {
+		netAddr, err := net.ResolveTCPAddr("tcp4", a)
+		if err != nil {
+			return nil, err
+		}
+		netAddrs[i] = netAddr
+	}
+	return netAddrs, nil
 }
